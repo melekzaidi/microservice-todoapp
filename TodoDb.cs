63f0085e5b9351e1fbce115fe82fb06c4238ff3e
melekzaidi <melekzaidimk@gmail.com>
2024-09-28 21:48:34 +0100

    Ajoutez des fichiers projet.

---
+++ TodoDb.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace microservice;
+
+public class TodoDb:DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options):base(options) { }
+    public DbSet<TodoItem> Todos { get; set; }    
+}
